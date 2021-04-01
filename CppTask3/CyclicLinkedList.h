c5d665bd4c8b7db051e832cce9782edd38e1e7a8
AntonovAleksandr <zzzz1917@bk.ru>
2021-04-01 16:57:00 +0300

    logic update

--- CppTask3/CyclicLinkedList.h
+++ CppTask3/CyclicLinkedList.h
@@ -13,41 +13,131 @@ class CyclicLinkedList {
 private:
     ListElement *root;
 public:
-    CyclicLinkedList(const ListElement &root) {
-
+    CyclicLinkedList(CyclicLinkedList list) {
+        add(list);
     }
 
+    CyclicLinkedList(const ListElement &root) {
+        root = root;
+    }
+
+    CyclicLinkedList() {
+        root = nullptr;
+    }
+
+    void add(CyclicLinkedList list) {
+        if(list.isEmpty()) return;
+        ListElement *target =  list.root;
+        ListElement *current =  target->next;
+        if(current == target){
+            add(target);
+        } else
+        while (current!=target){
+            add(current);
+            current = current->next;
+        }
+    }
+
+    //completed
     void add(T num) {
         ListElement *listElement = new ListElement(num);
-        if(root == nullptr)
+
+        if(isEmpty())
         {
             root = listElement;
             root.next = root;
         }
-        else                 //если узел уже не первый
+        else
         {
             ListElement *current = root;
-            while(current->next != nullptr)
+            while(current->next != root)
                 current = current->next;
-            //предшествующий указывает на последний
-            current->next = nd;
+            current->next = listElement;
         }
     }
 
+    //completed
     void remove(T i){
+        if(isEmpty()) return;
+
+        ListElement *current = root;
+        while (current->next != root){
+            if(current.data == i){
+                return current;
+            }
+            current = current->next;
+        }
 
     };
+
+    //completed
     void removeByIndex(int i){
 
+        if(isEmpty()) return;
+
+        ListElement *current = root;
+        for(int j = 0; j < i-1; j++){
+            current = current->next;
+        }
+        ListElement *removeElement = current->next;
+        current->next = removeElement->next;
     };
+
+    //completed
     void insertInto(int i, T element){
+        ListElement *listElement = new ListElement(num);
+        if(isEmpty())
+        {
+            root.next = root;
+            root = listElement;
+        }
+        else
+        {
+            ListElement *current = root;
+            for(int j = 0; j < i; j++){
+
+                current = current->next;
+            }
+            listElement->next = current->next;
+            current->next = listElement;
+        }
+    };
+
+
+    int getIndex(T i){
+        if(isEmpty()) return;
+
+        ListElement *current = root;
+        if(root->next != root){
+            if(current.data == i){
+                return current;
+            }
+            while (current->next != root){
+
+                current = current->next;
+            }
+        } else {
+
+        }
 
     };
-    ListElement get(T i){
 
-    };
-    ListElement getByIndex(T i){
+    //completed
+    ListElement get(int target){
+        if(isEmpty()) return;
 
+        ListElement *current = root;
+        for(int i = 0; i < target; i++){
+            current = current->next;
+        }
+        return current;
     };
+
+    bool isEmpty(){
+        if(root == nullptr) return true;
+        return false;
+    }
+
+
 
 };
